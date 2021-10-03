fd19c9d69b1c90c476595db4efdf439633a4189a
Jacob Wise <jacob@craftedup.com>
2021-10-03 19:55:27 -0400

    hardcode GTM_ID for repro

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import { Partytown, GoogleTagManager, GoogleTagManagerNoScript } from '@builder.io/partytown/react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { GTM_ID } from '../lib/gtm'
+// import { GTM_ID } from '../lib/gtm'
+const GTM_ID = 'GTM-KNZPMFF';
 console.log(GTM_ID)
 export default class MyDocument extends Document {
   render() {
