bf3176ee9b5d2f3ad670d253ef1fa0f3a1255922
Jacob Wise <jacob@craftedup.com>
2021-10-03 19:52:05 -0400

    remove analytics from tag manager and remove pageview

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,22 @@
 import '../styles/globals.css'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
-import { GTM_ID, pageview } from '../lib/gtm'
-import Script from 'next/script'
+// import { pageview } from '../lib/gtm'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
-  useEffect(() => {
-    router.events.on('routeChangeComplete', () => {
-      console.log('routeChangeComplete on')
-      pageview
-    })
-    return () => {
-      router.events.off('routeChangeComplete', () => {
-        console.log('routeChangeComplete off')
-        pageview
-      })
-    }
-  }, [router.events])
+  // useEffect(() => {
+  //   router.events.on('routeChangeComplete', () => {
+  //     console.log('routeChangeComplete on')
+  //     pageview
+  //   })
+  //   return () => {
+  //     router.events.off('routeChangeComplete', () => {
+  //       console.log('routeChangeComplete off')
+  //       pageview
+  //     })
+  //   }
+  // }, [router.events])
   return (
     <>
       <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,17 @@
-import { Partytown } from '@builder.io/partytown/react';
+import { Partytown, GoogleTagManager, GoogleTagManagerNoScript } from '@builder.io/partytown/react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-
+import { GTM_ID } from '../lib/gtm'
+console.log(GTM_ID)
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <Partytown />
+          <GoogleTagManager containerId={GTM_ID} />
+          <Partytown debug={true} />
         </Head>
         <body>
+          <GoogleTagManagerNoScript containerId={GTM_ID} />
           <Main />
           <NextScript />
         </body>
